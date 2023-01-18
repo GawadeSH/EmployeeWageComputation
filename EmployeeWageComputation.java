db74b891944b807fb5874f9fbb946a2582227ffe
shital gawade <shitalgawade131@gmail.com>
2023-01-18 19:58:21 +0530

    check attendace : present or absent

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,8 +1,25 @@
-public class EmployeeWageComputation
-{
-    public static void main(String[] args)
-    {
+public class EmployeeWageComputation {
+    public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+        EmployeeWageComputation E = new EmployeeWageComputation();
+        E.Attendance();
+
+    }
+
+
+    //Check Employee is Present or Absent
+
+    int P = 1;
+
+    void Attendance() {
+        int randomCheck = (int) (Math.random() * 2);
+        if (P == randomCheck) {
+            System.out.println("Employee is present");
+        } else {
+            System.out.println("Employee is absent");
+        }
 
     }
 }
+
+
