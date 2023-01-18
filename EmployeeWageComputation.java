1522e29872f2f232254fe499bd1d8f1d1c20f2e8
shital gawade <shitalgawade131@gmail.com>
2023-01-18 20:16:53 +0530

    Salary

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -2,24 +2,40 @@ public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
         EmployeeWageComputation E = new EmployeeWageComputation();
+
         E.Attendance();
+        E.Salary();
+
 
     }
-
-
     //Check Employee is Present or Absent
 
     int P = 1;
+    int randomCheck = (int) (Math.random() * 2);
 
     void Attendance() {
-        int randomCheck = (int) (Math.random() * 2);
+
         if (P == randomCheck) {
             System.out.println("Employee is present");
         } else {
             System.out.println("Employee is absent");
         }
+    }
+
+    //Calculate Daily Employee Wage
+    int empRatePerHrs = 20;
+    int empHr = 8;
+    void Salary() {
+        if (P == randomCheck) {
+
+            int salary = (empRatePerHrs * empHr);
+            System.out.println("Salary:-" + salary);
+        } else {
+            System.out.println("salary=0");
+        }
+
 
     }
+
+
 }
-
-
